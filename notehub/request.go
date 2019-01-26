c09c56420be1e46fda806b90728e39159bc17207
Ray Ozzie <ray@ozzie.net>
2019-01-26 14:05:30 -0500

    m

--- notehub/request.go
+++ notehub/request.go
@@ -10,11 +10,32 @@ import (
 
 // Supported requests
 
+// HubQuery (golint)
+const HubQuery          = "hub.app.data.query"
+// HubAppUpload (golint)
+const HubAppUpload      = "hub.app.upload.add"
+// HubAppUploads (golint)
+const HubAppUploads     = "hub.app.upload.query"
+// HubAppUploadSet (golint)
+const HubAppUploadSet	= "hub.app.upload.set"
+// HubAppUploadDelete (golint)
+const HubAppUploadDelete = "hub.app.upload.delete"
+// HubAppUploadRead (golint)
+const HubAppUploadRead  = "hub.app.upload.get"
+// HubAppMonitor (golint)
+const HubAppMonitor		= "hub.app.monitor"
+// HubAppHandlers (golint)
+const HubAppHandlers	= "hub.app.handlers"
+
 // HubRequest is is the core data structure for notehub-specific requests
 type Request struct {
 	note.Request				`json:",omitempty"`
+	AppUID string				`json:"app,omitempty"`
+	FleetUID string				`json:"fleet,omitempty"`
 	*DbQuery					`json:",omitempty"`
 	Uploads *[]RequestFile		`json:"uploads,omitempty"`
+	Contains string				`json:"contains,omitempty"`
+	Handlers *[]string			`json:"handlers,omitempty"`
 }
 
 // HubRequestFile is is the body of the object uploaded for each file
