45941b9f6aca4ce66202f70c3cc2389b255d14fc
Ray Ozzie <ray@ozzie.net>
2019-06-07 14:19:33 -0700

    implement filetype for uploads

--- notehub/request.go
+++ notehub/request.go
@@ -40,7 +40,7 @@ type HubRequest struct {
 	Uploads *[]HubRequestFile	`json:"uploads,omitempty"`
 	Contains string				`json:"contains,omitempty"`
 	Handlers *[]string			`json:"handlers,omitempty"`
-    Card bool					`json:"card,omitempty"`
+    FileType string				`json:"type,omitempty"`
 }
 
 // File Types
