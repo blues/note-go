97b5ddb1e43e895bbe6a2fec7f388a0ef384e2dc
Carlton Henderson <henderson.carlton@gmail.com>
2020-05-21 10:59:32 -0400

    fix: better release taging and archive names

--- package.sh
+++ package.sh
@@ -13,11 +13,12 @@ cd "$SCRIPT_DIR" # cd to this script's dir
 #
 # note-go package.sh
 #
-# This script packages all the note-go executables (note, notecard, notehub)
-# into an archive named note-go.$GOOS.$GOARCH.tar.gz or zip in the case of
-# $GOOS=windows.
+# This script packages the best note-go executables (notecard, notehub) into
+# archives named note{card,hub}cli_${GOOS}_${GOARCH}.tar.gz or .zip in the case of
+# ${GOOS}=windows. To be more user-friendly we call darwin 'macos' in the archive
+# names.
 #
-# Parameters: This script uses $GOOS and $GOARCH determine where to look for the
+# Parameters: This script uses ${GOOS} and ${GOARCH} determine where to look for the
 #             executables.
 #
 # Output: Archives are saved in "./build/packages/"
@@ -26,15 +27,20 @@ cd "$SCRIPT_DIR" # cd to this script's dir
 # Add GOOS and GOARCH to our environment. (and other GO vars we don't need)
 eval "$(go env)"
 
-readonly BUILD_EXE_DIR="$SCRIPT_DIR/build/$GOOS/$GOARCH/"
+readonly BUILD_EXE_DIR="$SCRIPT_DIR/build/${GOOS}/${GOARCH}/"
 mkdir -p "$BUILD_EXE_DIR"
 readonly BUILD_PACKAGE_DIR="$SCRIPT_DIR/build/packages/"
 mkdir -p "$BUILD_PACKAGE_DIR"
 
 # compress the build products into an archive
 cd "$BUILD_EXE_DIR"
-if [ "$GOOS" = "windows" ]; then
-  zip "$BUILD_PACKAGE_DIR/note-go.$GOOS.$GOARCH.zip" ./*
+if [ "${GOOS}" = "windows" ]; then
+  zip "$BUILD_PACKAGE_DIR/notecardcli_${GOOS}_${GOARCH}.zip" ./notecard
+  zip "$BUILD_PACKAGE_DIR/notehubcli_${GOOS}_${GOARCH}.zip" ./notehub
+elif [ "${GOOS}" = "darwin" ]; then
+  tar -czvf "$BUILD_PACKAGE_DIR/notecardcli_macos_${GOARCH}.tar.gz" ./notecard
+  tar -czvf "$BUILD_PACKAGE_DIR/notehubcli_macos_${GOARCH}.tar.gz" ./notehub
 else
-  tar -czvf "$BUILD_PACKAGE_DIR/note-go.$GOOS.$GOARCH.tar.gz" ./*
+  tar -czvf "$BUILD_PACKAGE_DIR/notecardcli_${GOOS}_${GOARCH}.tar.gz" ./notecard
+  tar -czvf "$BUILD_PACKAGE_DIR/notehubcli_${GOOS}_${GOARCH}.tar.gz" ./notehub
 fi;
